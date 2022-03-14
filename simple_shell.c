7c36e870e117e81f2b46598370ac5339ace72c13
zkarach1 <karachiwallazulekha@gmail..com>
2022-03-14 14:50:38 -0400

    new commit with file reading

--- simple_shell.c
+++ simple_shell.c
@@ -175,7 +175,37 @@ int starting_process(char **tokens)
         char *result = malloc(len1 +len2 +1);
         memcpy(result, string, len1);
         memcpy(result + len1, tokens[1], len2 + 1);
-        printf("%s", result);
+        //printf("%s", result);
+
+        FILE *fp = fopen(result, "r");
+
+        if (!fp)
+        {
+            printf("error in opening file");
+            return 1;
+        }
+
+        char *line_buf = NULL;
+        size_t line_buf_size = 0;
+        int line_count = 0;
+        ssize_t line_size;
+
+        line_size = getline(&line_buf, &line_buf_size, fp);
+
+          /* Loop through until we are done with the file. */
+        while (line_size >= 0)
+        {
+         /* Increment our line count */
+         line_count++;
+
+        /* Show the line details */
+        printf("line[%06d]: chars=%06zd, buf size=%06zu, contents: %s", line_count,
+        line_size, line_buf_size, line_buf);
+
+        /* Get the next line */
+        line_size = getline(&line_buf, &line_buf_size, fp);
+        
+        }
 
         //open file
         //strcat(string,tokens[1]);
